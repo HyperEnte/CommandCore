456c46bdde263c1c9ba0fa810916b0d8ced848b3
HyperEnte <54057755+HyperEnte-PHP@users.noreply.github.com>
2020-02-25 14:58:46 +0100

    Add /announce

--- CommandCore/src/HyperEnte/CommandCore/CommandCore.php
+++ CommandCore/src/HyperEnte/CommandCore/CommandCore.php
@@ -5,6 +5,7 @@ namespace HyperEnte\CommandCore;
 use HyperEnte\CommandCore\commands\HeadOrNumberCommand;
 use pocketmine\plugin\PluginBase;
 use HyperEnte\CommandCore\commands\SizeCommand;
+use HyperEnte\CommandCore\commands\AnnounceCommand;
 use HyperEnte\CommandCore\forms\SizeForm;
 use pocketmine\Server;
 
@@ -16,7 +17,7 @@ class CommandCore extends PluginBase{
     public function onEnable()
     {
         self::$main = $this;
-        $this->getServer()->getCommandMap()->registerAll("CommandCore", [new SizeCommand(), new HeadOrNumberCommand()]);
+        $this->getServer()->getCommandMap()->registerAll("CommandCore", [new SizeCommand(), new HeadOrNumberCommand(), new AnnounceCommand()]);
     }
     public static function getMain(): self{
         return self::$main;
