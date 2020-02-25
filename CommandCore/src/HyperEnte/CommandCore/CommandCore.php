e39bbe793d2e7fecf21541e246614332295084e2
HyperEnte <54057755+HyperEnte-PHP@users.noreply.github.com>
2020-02-25 17:10:55 +0100

    Add /fly and /vanish

--- CommandCore/src/HyperEnte/CommandCore/CommandCore.php
+++ CommandCore/src/HyperEnte/CommandCore/CommandCore.php
@@ -6,18 +6,20 @@ use HyperEnte\CommandCore\commands\HeadOrNumberCommand;
 use pocketmine\plugin\PluginBase;
 use HyperEnte\CommandCore\commands\SizeCommand;
 use HyperEnte\CommandCore\commands\AnnounceCommand;
+use HyperEnte\CommandCore\commands\FlyCommand;
+use HyperEnte\CommandCore\commands\VanishCommand;
 use HyperEnte\CommandCore\forms\SizeForm;
 use pocketmine\Server;
 
 class CommandCore extends PluginBase{
     public const PREFIX = "§3CommandCore §8>> §r";
-    public const NOPERM = self::PREFIX."§cDazu hast du keine Berechtigung";
-    private static $main;
+    public const NOPERM = self::PREFIX."§cYou have no permission doing this";
+    public static $main;
 
     public function onEnable()
     {
         self::$main = $this;
-        $this->getServer()->getCommandMap()->registerAll("CommandCore", [new SizeCommand(), new HeadOrNumberCommand(), new AnnounceCommand()]);
+        $this->getServer()->getCommandMap()->registerAll("CommandCore", [new SizeCommand(), new HeadOrNumberCommand(), new AnnounceCommand(), new FlyCommand(), new VanishCommand()]);
     }
     public static function getMain(): self{
         return self::$main;
